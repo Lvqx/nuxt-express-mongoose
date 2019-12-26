c67ce30b43b8e5cdc58282651e904ed88ceb38d8
lvqx <betterlvqx@163.com>
2019-12-26 09:38:31 +0800

    详情

---
+++ pages/detail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>{{ detail.name }}</div>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  data() {
+    return {
+      detail: {
+        name: ""
+      }
+    };
+  },
+  mounted() {
+    this.initDetail();
+  },
+  methods: {
+    initDetail() {
+      const { id } = this.$route.query;
+      this.$axios.get(`/home/detail/${id}`).then(({ data: res } = response) => {
+        this.detail = res.data;
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>>
